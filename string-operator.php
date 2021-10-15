31adbee41821030c41eee9459c3458cc507a39c8
Shariflock <shariflocked@gmail.com>
2021-10-15 22:54:09 +0600

    string-operator
    Two operator for string data type:
    1. Dot (' . ') -> concatenation operator
    2. (' .= ') -> concatenation assignment operator.

---
+++ string-operator.php
@@ -0,0 +1,16 @@
+<?php
+
+$a = 200;
+
+$s = $a . " world " . 500;
+
+echo $s;
+
+echo "</br>";
+
+$x = "My Name is Sharif, ";
+$x .=" I am 25 years old, ";
+$x .=  555 ;
+$x .=" I am a Designer";
+
+echo $x;
