ec334b8a475ae5bbc4c90e357bf94788530fe67e
Shariflock <shariflocked@gmail.com>
2021-10-15 23:57:25 +0600

    Do-while-loop
    Condition is checked after the loop's execution.

---
+++ do-while-loop.php
@@ -0,0 +1,18 @@
+<?php
+
+$b = 1;
+
+do{
+    echo $b. ") This is new football team <br>";
+    $b++;
+}while($b <= 10);
+
+
+echo "<br>";
+
+$c = 10;
+
+do{
+    echo $c. ") This is new football team <br>";
+    $c--;
+}while($c >= 1);
