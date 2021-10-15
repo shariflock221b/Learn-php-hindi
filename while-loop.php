c5b10c0cb3e7430497f108b2145c377a89765223
Shariflock <shariflocked@gmail.com>
2021-10-15 23:41:55 +0600

    Php-while-loop
    php while loop can be used to traverse set of code like for. it's called an entry control loop. that means that  first the condition is checked. if the condition is true , the block of code will be executed.

---
+++ while-loop.php
@@ -0,0 +1,36 @@
+<?php
+
+$a = 1;
+
+while($a <= 10){
+    echo $a. ") Hellow my name is sharif </br>";
+    // $a = $a + 1;
+
+    // echo $a."</br>";
+    $a++;
+}
+
+echo "<br>";
+
+$b = 10;
+
+while($b >= 1){
+    echo $b.") Hellow world <br>";
+    // echo $b."<br>";
+    $b--;
+}
+
+
+$c = 1;
+echo "<ul>";
+
+while($c <= 10){
+    echo "<li>". $c. " Hellow world </li>";
+    // echo $b."<br>";
+    //$c++;
+    $c = $c + 2;
+
+    
+}
+
+echo "</ul>";
