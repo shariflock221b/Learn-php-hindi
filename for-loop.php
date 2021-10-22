3c9bbfa038a47be20f18052198cd65477cb687e1
Shariflock <shariflocked@gmail.com>
2021-10-22 12:15:02 +0600

    For-Loop
    A "For" Loop is used to repeat a specific block of code a known number of times

---
+++ for-loop.php
@@ -0,0 +1,16 @@
+<?php
+
+for($i = 1; $i <= 10; $i++) {
+    echo $i . ") I am Shariful Islam<br>";
+
+}
+
+echo "<hr>";
+for($j = 1; $j <= 10; $j = $j + 2) {
+    echo $j . ") I am Shariful Islam<br>";
+}
+
+echo "<hr>";
+for($j = 10; $j >= 1; $j--) {
+    echo $j . ") I am Shariful Islam<br>";
+}
