b89dc49ec561ec8f9f2a7f02eac6a950d51b2bf6
Shariflock <shariflocked@gmail.com>
2021-10-23 19:03:00 +0600

    Goto
    The goto statement is used to send flow of the program to a certain location in the code

---
+++ goto.php
@@ -0,0 +1,12 @@
+<?php
+echo "I am shariful Islam<br>";
+echo "I am shariful Islam <br>";
+echo "I am shariful Islam <br>";
+goto abc;
+echo "I am shariful Islam <br>";
+echo "I am shariful Islam <br>";
+echo "I am shariful Islam<br>";
+echo "I am shariful Islam<br>";
+
+abc:
+echo "I am abc model";
