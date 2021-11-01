a0da2e9dc2012230bd74be27a5b4bdc00f57d166
Shariflock <shariflocked@gmail.com>
2021-11-01 21:56:45 +0600

    foreach loop
    PHP foreach loop is utilized for looping through the values of an array

---
+++ foreach.php
@@ -0,0 +1,22 @@
+<?php
+
+$colors = ["red", "green", "Blue", "Yellow"];
+
+$colors = [
+
+     "red" => 1,
+     "green" => 2,
+     "Blue" => 3,
+     "Yellow" => 4
+];
+
+
+foreach($colors as $key => $value) {
+    echo $key . " = ". $value . " <br>";
+    echo  $key . " = ". $value . " <br>";
+}
+
+
+// for($i = 0; $i < 4; $i++) {
+//     echo $colors[$i] . "<br>";
+// }
