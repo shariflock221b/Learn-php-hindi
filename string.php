d764d3a9a5ea1fe0955291405277d0bac88c6054
Shariflock <shariflocked@gmail.com>
2021-11-16 19:17:01 +0600

    wordwrap,min,max, floor,ceil,round,abs

--- string.php
+++ string.php
@@ -58,11 +58,47 @@
 
 // echo $decode;
 
- $not = hex2bin("hellow world") . "<br>";
+//  $not = hex2bin("hellow world") . "<br>";
 
-echo $not;
-echo hex2bin($not);
+// echo $not;
+// echo hex2bin($not);
 
-$lol = chr(3);
-$lol = ord("A");
-echo $lol;
+// $lol = chr(3);
+// $lol = ord("A");
+// echo $lol;
+
+// $str = "I am shariful islam";
+
+// echo wordwrap($str, 4, "<br>", true);
+
+// $val = min(array(3,4,5,6), array(5,6,4,3));
+// $val = max(array(3,4,5,6), array(5,6,4,3));
+
+// echo "<pre>";
+// print_r($val);
+
+// echo "</pre>";
+
+echo (ceil(0.60). "<br>");
+echo (ceil(0.40). "<br>");
+echo (ceil(5). "<br>");
+echo (ceil(5.1). "<br>");
+echo (ceil(-5.1). "<br>");
+
+echo (floor(0.60). "<br>");
+echo (floor(0.40). "<br>");
+echo (floor(5). "<br>");
+echo (floor(5.1). "<br>");
+echo (floor(-5.1). "<br>");
+
+echo (round(0.60). "<br>");
+echo (round(0.40). "<br>");
+echo (round(5). "<br>");
+echo (round(5.1). "<br>");
+echo (round(-5.1). "<br>");
+
+echo (abs(0.60). "<br>");
+echo (abs(0.40). "<br>");
+echo (abs(5). "<br>");
+echo (abs(5.1). "<br>");
+echo (abs(-5.1). "<br>");
