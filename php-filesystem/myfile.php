fe5e584adf112419faec6d86827e9da3ca685d22
Shariful Islam <shariflocked@gmail.com>
2022-02-17 22:13:53 +0600

    Php-file system

--- php-filesystem/myfile.php
+++ php-filesystem/myfile.php
@@ -1,17 +1,17 @@
 <?php
 
-$file = "readme.txt";
+ //$file = "readme.txt";
 
-$path = realpath($file);
-echo "<pre>";
-print_r(pathinfo($path,PATHINFO_BASENAME));
-print_r(pathinfo($path,PATHINFO_DIRNAME));
-print_r(pathinfo($path,PATHINFO_EXTENSION));
-print_r(pathinfo($path,PATHINFO_FILENAME));
-echo "</pre>";
+// $path = realpath($file);
+// echo "<pre>";
+// print_r(pathinfo($path,PATHINFO_BASENAME));
+// print_r(pathinfo($path,PATHINFO_DIRNAME));
+// print_r(pathinfo($path,PATHINFO_EXTENSION));
+// print_r(pathinfo($path,PATHINFO_FILENAME));
+// echo "</pre>";
 
-echo basename($path, ".txt");
-echo dirname($path);
+// echo basename($path, ".txt");
+// echo dirname($path);
 // if(file_exists($file)){
 //     // echo readfile("readme.txt");
 //     // copy($file, "newfile.txt");
@@ -31,3 +31,51 @@ echo dirname($path);
 // echo filetype($file);
 //echo realpath($file);
 //echo pathinfo($file);
+
+$file = fopen("readme.txt", "a+");
+
+// // echo fread($file, 50);
+// // echo fread($file, filesize("readme.txt"));
+// echo fgets($file);
+// echo ftell($file);
+//echo fseek($file,15);
+// echo fpassthru($file);
+// rewind($file);
+ //echo "<br><br>" . fgets($file);
+
+//     echo "<ul>";
+//  while(!feof($file)){
+//      $line = fgets($file);
+//      echo "<li>". $line . "</li>";
+//  }
+
+//  echo "</ul>";
+
+// fwrite($file, "\nHere is new line");
+ftruncate($file, 50);
+
+if(is_file("readme.txt")){
+    echo "Yes it is file";
+}else {
+    echo "No it is not a file";
+}
+echo "<br>";
+if(is_dir("readme.txt")){
+    echo "Yes it is Folder";
+}else {
+    echo "No it is not a Folder";
+}
+
+echo "<br>";
+if(is_writeable("readme.txt")){
+    echo "Yes it is writeable";
+}else {
+    echo "No it is not a writeable";
+}
+
+echo "<br>";
+if(is_executable("readme.txt")){
+    echo "Yes it is executable";
+}else {
+    echo "No it is not a executable";
+}
